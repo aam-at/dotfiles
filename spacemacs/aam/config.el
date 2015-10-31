ab2b6f81d39e605a6eede75c3bca01609402a3cd
Alexander Matyasko <alexander.matyasko@gmail.com>
2015-10-31 14:54:46 +0800

    Updated config for google calendar (I don't worry about exposing calendar id since calendar is private).

--- spacemacs/aam/config.el
+++ spacemacs/aam/config.el
@@ -2,9 +2,6 @@
 ;;       '((:calendar-id "Projects.org" :files ("~/Dropbox/Notes/Projects.org")
 ;;                       :inbox "~/Dropbox/Notes/fromwork.org")))
 (setq org-caldav-calendars
-      '((:calendar-id "work@whatever" :files ("~/org/work.org")
-                      :inbox "~/org/fromwork.org")
-        (:calendar-id "stuff@mystuff"
-                      :files ("~/org/sports.org" "~/org/play.org")
-                      :inbox "~/org/fromstuff.org")) )
-
+      '((:calendar-id "iu5alt927aue6hsjis25qhsark@group.calendar.google.com" :files ("~/Dropbox/Notes/work.org")
+                      :inbox "~/Dropbox/Notes/fromwork.org"))
+      )
