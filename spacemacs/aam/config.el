7b87de5a7bc963e53d9414cffedc3c9c172853b6
Alexander Matyasko <alexander.matyasko@gmail.com>
2015-10-31 15:21:44 +0800

    Removed comment. Updated environment Added stub for keybindings in my layer.

--- spacemacs/aam/config.el
+++ spacemacs/aam/config.el
@@ -1,6 +1,3 @@
-;; (setq org-caldav-calendars
-;;       '((:calendar-id "Projects.org" :files ("~/Dropbox/Notes/Projects.org")
-;;                       :inbox "~/Dropbox/Notes/fromwork.org")))
 (setq org-caldav-calendars
       '((:calendar-id "iu5alt927aue6hsjis25qhsark@group.calendar.google.com" :files ("~/Dropbox/Notes/work.org")
                       :inbox "~/Dropbox/Notes/fromwork.org"))
