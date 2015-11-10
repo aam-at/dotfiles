548b5f8936a375e02e9ca8bbc7d3cb48387dd297
Alexander Matyasko <alexander.matyasko@gmail.com>
2015-11-10 16:15:41 +0800

    Updated script for windows emacs.

--- install_simple.bat
+++ install_simple.bat
@@ -2,3 +2,4 @@ mklink /H ..\_vimrc vim\vimrc
 mklink /H ..\_gvimrc vim\gvimrc
 mklink /H ..\.ideavimrc idea\ideavimrc
 mklink /H ..\.spacemacs spacemacs\spacemacs
+mklink /H ..\.emacs.d\private spacemacs\aam
