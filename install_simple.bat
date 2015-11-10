34387cd75b58979cfd11de65e87e1f897f643f68
Alexander Matyasko <alexander.matyasko@gmail.com>
2015-11-10 16:18:39 +0800

    Updated config for windows.

--- install_simple.bat
+++ install_simple.bat
@@ -1,5 +1,6 @@
 mklink /H ..\_vimrc vim\vimrc
 mklink /H ..\_gvimrc vim\gvimrc
 mklink /H ..\.ideavimrc idea\ideavimrc
+mklink /H ..\.gitconfig git\gitconfig
 mklink /H ..\.spacemacs spacemacs\spacemacs
 mklink /H ..\.emacs.d\private spacemacs\aam
