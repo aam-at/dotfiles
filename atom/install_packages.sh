9f7228338a1d425ce2298c04a9b57583306b3fe8
Alexander Matyasko <alexander.matyasko@gmail.com>
2016-04-25 23:56:53 +0800

    Atom: add lualinter luacheck

--- atom/install_packages.sh
+++ atom/install_packages.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-apm install advanced-open-file autocomplete-paths emacs-plus ex-mode Hydrogen language-ipynb language-lua project-manager script vim-mode
+apm install advanced-open-file autocomplete-paths emacs-plus ex-mode Hydrogen language-ipynb language-lua linter-luacheck project-manager script vim-mode
