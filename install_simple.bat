371f95649d12e1613ac8d4cc3676f30f7441885b
Alexander Matyasko <alexander.matyasko@gmail.com>
2016-05-19 14:11:12 +0800

    Windows: updates linking bat

--- install_simple.bat
+++ install_simple.bat
@@ -1,8 +1,11 @@
 mklink /H ..\_vimrc vim\vimrc
+mklink /J ..\.vim\bundle\neobundle.vim vim\bundle\neobundle.vim
 mklink /H ..\_gvimrc vim\gvimrc
 mklink /H ..\.ideavimrc idea\ideavimrc
 mklink /H ..\.gitconfig git\gitconfig
 mklink /H ..\.spacemacs spacemacs\spacemacs
+mklink /H ..\.emacs.d\core\secure-config.el.gpg spacemacs\secure-config.el.gpg
+
 mklink /J ..\.emacs.d\private\aam spacemacs\aam
 mklink /J ..\.emacs.d\private\notmuch spacemacs\notmuch
 mklink /J ..\.emacs.d\private\org-extras spacemacs\org-extras
