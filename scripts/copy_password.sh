7c7396d099943d06a523e0b1aa7dd341db35a505
Alexander Matyasko <alexander.matyasko@gmail.com>
2018-04-23 12:14:49 +0800

    Bash: add function and script to copy password from authinfo.gpg

---
+++ scripts/copy_password.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+if [ $# -eq 0 ]
+then
+    echo "No arguments supplied"
+    exit 1;
+fi
+
+login="$1"
+
+echo $(gpg2 -q --for-your-eyes-only --no-tty -d ~/.authinfo.gpg | awk '/login '$1' .*password/ {print $NF}')
