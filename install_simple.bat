beeef5a80bac7cad0d6e5220e8975d2b291956ab
Alexander Matyasko <alexander.matyasko@gmail.com>
2018-05-27 08:53:14 +0800

    Windows: update linking script

--- install_simple.bat
+++ install_simple.bat
@@ -5,14 +5,3 @@ mklink /H ..\.ideavimrc idea\ideavimrc
 mklink /H ..\.gitconfig git\gitconfig
 mklink /H ..\.spacemacs spacemacs\spacemacs
 mklink /H ..\.emacs.d\core\secure-config.el.gpg spacemacs\secure-config.el.gpg
-
-mklink /J ..\.emacs.d\private\aam spacemacs\aam
-mklink /J ..\.emacs.d\private\emacs-icons spacemacs\emacs-icons
-mklink /J ..\.emacs.d\private\emms spacemacs\emms
-mklink /J ..\.emacs.d\private\habitrpg spacemacs\habitrpg
-mklink /J ..\.emacs.d\private\notmuch spacemacs\notmuch
-mklink /J ..\.emacs.d\private\org-extras spacemacs\org-extras
-mklink /J ..\.emacs.d\private\journal spacemacs\org-journal
-mklink /J ..\.emacs.d\private\python-extras spacemacs\python-extras
-mklink /J ..\.emacs.d\private\realgud spacemacs\realgud
-mklink /J ..\.emacs.d\private\writing spacemacs\writing
