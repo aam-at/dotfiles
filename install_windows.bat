8e29ead7afeeceaf6e137aeaa0b216cb1939f750
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-01-22 16:30:28 +0800

    Install: update script for windows

--- install_windows.bat
+++ install_windows.bat
@@ -1,8 +1,8 @@
-choco install 7zip adb ag autohotkey calibre curl ^
+choco install 7zip adb ag adobereader autohotkey calibre curl ^
               dropbox emacs evernote ffmpeg firefox ^
               git gnupg google-backup-and-sync ^
-              googlechrome gpg4win handbrake jre8 ^
+              googlechrome gpg4win handbrake jre8 msys2 ^
               notepadplusplus pandoc python python3 ^
-              qtpass spotify sysinternals telegram ^
+              qtpass skype spotify sysinternals telegram ^
               totalcommander utorrent viber vim ^
               virtualbox vlc wget winrar WinFsp youtube-dl
