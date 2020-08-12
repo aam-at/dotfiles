2dc7abd5813ff882cac924c748cb7cef547f7933
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-08-12 18:04:16 +0800

    Emacs: update bibtex and org-roam packages configs

--- spacemacs/aam/funcs.el
+++ spacemacs/aam/funcs.el
@@ -7,3 +7,10 @@
     (when filename
       (kill-new filename)
       (message "Copied buffer file name '%s' to the clipboard." filename))))
+
+(defun aam-open-pdf-external (key)
+  (start-process "xournal" "*helm-bibtex-xournal*" "/usr/bin/xournal"
+                 (expand-file-name (aam-get-cite-pdf-filename (car key)))))
+
+(defun aam-get-cite-pdf-filename (key)
+  (concat org-ref-pdf-directory (format "%s.pdf" key)))
