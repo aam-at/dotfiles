828ba798f04d70d674e0bff3094abc7aa08bd374
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-08-30 21:03:07 +0800

    Emacs: update config for windows

--- install_windows.bat
+++ install_windows.bat
@@ -3,6 +3,6 @@ choco install 7zip adb ag adobereader autohotkey calibre curl ^
               git gnupg google-backup-and-sync ^
               googlechrome gpg4win handbrake jre8 msys2 ^
               notepadplusplus pandoc python python3 ^
-              qtpass skype spotify sysinternals telegram ^
+              qtpass sqlite skype spotify sysinternals telegram ^
               totalcommander utorrent viber vim ^
               virtualbox vlc wget winrar WinFsp youtube-dl
