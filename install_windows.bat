9d3555a9d7bc7a4f8fde4abd3e1594dbac44740a
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-09-23 10:24:54 +0800

    Windows: add clink install with choco

--- install_windows.bat
+++ install_windows.bat
@@ -1,5 +1,5 @@
 choco install ^
-7zip adb adobereader ag anki autohotkey calibre conemu curl dropbox emacs ^
+7zip adb adobereader ag anki autohotkey calibre clink conemu curl dropbox emacs ^
 evernote evince far ffmpeg firefox git gnupg google-backup-and-sync googlechrome ^
 gpg4win handbrake jabref jre8 latexdraw msys2 notepadplusplus pandoc python ^
 python3 qtpass ripgrep skype spotify sqlite sysinternals telegram totalcommander ^
