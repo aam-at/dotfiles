f0a987d84ff60775273c93233e54cb193d4c1c3b
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-10-22 02:41:19 +0800

    Emacs: org-roam has been added to spacemacs

--- spacemacs/org-roam/packages.el
+++ spacemacs/org-roam/packages.el
@@ -2,46 +2,9 @@
 
 (defconst org-roam-packages
   '(
-    org-roam
     org-roam-bibtex
     org-roam-server
     company-org-roam))
-
-(defun org-roam/init-org-roam ()
-  (use-package org-roam
-    :after org
-    :hook
-    (org-mode . org-roam-mode)
-    :init
-    (progn
-      (spacemacs/declare-prefix "aoR" "org-roam")
-      (spacemacs/set-leader-keys
-        "aoRl" 'org-roam
-        "aoRb" 'org-roam-switch-to-buffer
-        "aoRf" 'org-roam-find-file
-        "aoRF" 'org-roam-find-file-immediate
-        "aoRi" 'org-roam-insert
-        "aoRI" 'org-roam-insert-immediate
-        "aoRR" 'org-roam-random-note
-        "aoRg" 'org-roam-graph)
-      (spacemacs/declare-prefix-for-mode 'org-mode "aoR" "org-roam")
-
-      (defun aam/org-roam-init-keys (mode)
-        (progn
-          (spacemacs/declare-prefix-for-mode mode "mR" "org-roam")
-          (spacemacs/set-leader-keys-for-major-mode mode
-            "Rl" 'org-roam
-            "Rb" 'org-roam-switch-to-buffer
-            "Rf" 'org-roam-find-file
-            "RF" 'org-roam-find-file-immediate
-            "Ri" 'org-roam-insert
-            "RI" 'org-roam-insert-immediate
-            "RR" 'org-roam-random-note
-            "Rg" 'org-roam-graph)))
-
-      (aam/org-roam-init-keys 'org-mode)
-      (aam/org-roam-init-keys 'org-journal-mode))))
-
 
 (defun org-roam/init-org-roam-bibtex ()
   (use-package org-roam-bibtex
