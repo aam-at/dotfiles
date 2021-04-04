105eff0622ad1e42d3f2e0db99322f15f32fdf24
Alexander Matyasko <alexander.matyasko@gmail.com>
2021-04-04 12:53:42 +0100

    Update install script for ubuntu

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -8,16 +8,16 @@ sudo apt-get install apt-fast
 
 # install other packages
 sudo apt-fast install -y \
-    anki autojump automake bibtool build-essential checkinstall clang cmake cscope \
+    anki autojump automake bibtool build-essential checkinstall clang cmake code cscope \
     curl ditaa fasd fbreader fd-find fish fzy git glances global gnome-tweak-tool graphviz \
     guile-2.0-dev html2text iotop isync libbz2-dev libevent-dev libffi-dev \
     libgif-dev libgmime-3.0-dev libgnutls28-dev libjpeg-dev liblzma-dev \
-    libmagick++-dev libmagickcore-dev libncurses5-dev libncursesw5-dev libpng-dev \
+    libmagick++-dev libmagickcore-dev libncurses5-dev libncursesw5 libncursesw5-dev libpng-dev \
     libpoppler-glib-dev libpoppler-private-dev libreadline-dev libsqlite3-dev \
     libssl-dev libsystemd-dev libtiff-dev libwebkit2gtk-4.0-dev \
     libxapian-dev libxpm-dev llvm make mc mosh ncdu net-tools nnn openssh-server \
     pass plantuml pydf python-openssl ripgrep rtv rtv shellcheck texinfo tig tk-dev \
-    trash-cli wget wmctrl xdg-utils xz-utils zathura zathura-djvu \
+    tmux trash-cli wget wmctrl xdg-utils xz-utils zathura zathura-djvu \
     zathura-pdf-poppler zlib1g-dev zlib1g-dev
 
 sudo add-apt-repository ppa:git-core/ppa -y
@@ -53,14 +53,6 @@ if [ ! -d $HOME/.pyenv ]; then
     git clone git://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
 fi
 
-if [ ! -d $HOME/.pyenv/versions/2.7.17 ]; then
-    CONFIGURE_OPTS=--enable-shared pyenv install 2.7.17
-    # configure neovim
-    pyenv virtualenv 2.7.17 neovim2
-    pyenv activate neovim2
-    pip2 install pynvim
-fi
-
 if [ ! -d $HOME/.pyenv/versions/3.8.1 ]; then
     CONFIGURE_OPTS=--enable-shared pyenv install 3.8.1
 
@@ -89,18 +81,24 @@ fi
 mkdir -p ~/local/tools
 
 # download tmux
-if [ ! -f $HOME/local/tools/3.1b.tar.gz ]; then
-    wget https://github.com/tmux/tmux/archive/3.1b.tar.gz -P ~/local/tools
-fi
-
-# download fish
-if [ ! -f $HOME/local/tools/fish-3.1.2.tar.gz ]; then
-    wget https://github.com/fish-shell/fish-shell/releases/download/3.1.2/fish-3.1.2.tar.gz -P ~/local/tools
+if [ ! -f $HOME/local/tools/3.1.tar.gz ]; then
+    wget https://github.com/tmux/tmux/archive/refs/tags/3.1.tar.gz -P ~/local/tools
 fi
 
 # install enhancd for bash
 if [ ! -d $HOME/local/tools/enhancd-bash ]; then
     git clone https://github.com/b4b4r07/enhancd $HOME/local/tools/enhancd-bash
+fi
+
+# install pathpicker
+if ! [ -x "$(command -v fpp)" ]; then
+    git clone https://github.com/facebook/PathPicker.git /tmp/PathPicker
+    cd /tmp/PathPicker/debian
+    ./package.sh
+    cd ..
+    sudo dpkg -i "$(ls *.deb)"
+    cd ..
+    rm -rf PathPicker
 fi
 
 # install fzf
@@ -116,7 +114,12 @@ if [ ! -d $HOME/.config/omf ]; then
 fi
 
 # snap packages
+sudo snap refresh
 sudo snap install --classic skype
+sudo snap install --classic pycharm-professional
 sudo snap install opera
 sudo snap install spotify
 sudo snap install tusk
+
+# restore dir
+cd ~/dotfiles
