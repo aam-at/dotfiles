36f003e0377ed9ee1416f41242e6b36d41f9ab4d
Alexander Matyasko <alexander.matyasko@gmail.com>
2021-04-12 10:01:04 +0100

    Emacs: fix org-roam-server loading

--- spacemacs/org-roam/packages.el
+++ spacemacs/org-roam/packages.el
@@ -16,12 +16,8 @@
     :init
     (spacemacs/set-leader-keys-for-major-mode 'org-mode "N" 'orb-note-actions)))
 
-(defun org-roam/init-org-roam-server ()
-  (use-package org-roam-server
-    :after org-roam
-    :hook
-    (org-roam-mode . org-roam-server-mode)
-    :ensure t))
+(defun org-roam/post-init-org-roam-server ()
+  (add-hook 'org-mode-hook (lambda () (org-roam-server-mode))))
 
 (defun org-roam/post-init-org ()
   (require 'org-protocol)
