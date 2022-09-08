c24b59acb43e798582497099e9dda386d68d0b1b
Alexander Matyasko <alexander.matyasko@gmail.com>
2022-09-09 01:41:55 +0800

    Windows: add delta for git

--- install_windows.bat
+++ install_windows.bat
@@ -1,5 +1,5 @@
 choco install ^
-7zip adb adobereader ag anki autohotkey calibre clink conemu curl dropbox emacs ^
+7zip adb adobereader ag anki autohotkey calibre clink conemu curl delta dropbox emacs ^
 evernote evince far ffmpeg firefox git gnupg google-backup-and-sync googlechrome ^
 gpg4win handbrake jre8 latexdraw msys2 notepadplusplus pandoc python ^
 python3 qtpass ripgrep skype spotify sqlite sysinternals telegram totalcommander ^
