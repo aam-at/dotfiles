adf04e332232099defc52da70f96662ca52e1370
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-05-31 03:21:23 +0800

    System: use jetbrains mono as a default font

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -10,21 +10,22 @@ sudo apt-get install apt-fast
 sudo apt-fast install -y \
      alacritty anki autojump automake bibtool build-essential checkinstall \
      chrome-gnome-shell clang cmake cmake cscope curl curl ditaa fasd fbreader \
-     fd-find fish fonts-firacode fzy gcc-10 gettext git git-lfs glances global \
-     gnome-tweaks gnupg2 graphviz guile-3.0-dev html2text htop iotop \
-     iputils-arping isync jq keychain libbz2-dev libevent-dev libffi-dev \
-     libfontconfig1-dev libfreetype6-dev libgccjit-14-dev libgccjit0 libgif-dev \
-     libgmime-3.0-dev libgnutls28-dev libjansson-dev libjansson4 libjpeg-dev \
-     liblzma-dev libmagick++-dev libmagickcore-dev libncurses-dev libncurses6 \
-     libncursesw6 libpng-dev libpoppler-glib-dev libpoppler-private-dev \
-     libreadline-dev libsqlite3-dev libssl-dev libsystemd-dev libtiff-dev \
-     libwebkit2gtk-4.1-dev libxapian-dev libxcb-xfixes0-dev libxkbcommon-dev \
-     libxpm-dev llvm make mc meson mosh ncdu net-tools nnn notmuch \
-     openssh-server p7zip-full p7zip-rar pandoc pass peco pkg-config plantuml \
-     postfix pydf python3 python3-openssl python3-pip python3-pip ranger ripgrep \
-     rtv rtv ruby screen shellcheck sqlite3 stow tabbed texinfo tig tk-dev tmux \
-     trash-cli ubuntu-restricted-extras unrar wget wmctrl xdg-utils xdotool \
-     xz-utils zathura zathura-djvu zathura-pdf-poppler zlib1g-dev
+     fd-find fish fonts-firacode fonts-jetbrains-mono fzy gcc-10 gettext git \
+     git-lfs glances global gnome-tweaks gnupg2 graphviz guile-3.0-dev html2text \
+     htop iotop iputils-arping isync jq keychain kitty libbz2-dev libevent-dev \
+     libffi-dev libfontconfig1-dev libfreetype6-dev libgccjit-14-dev libgccjit0 \
+     libgif-dev libgmime-3.0-dev libgnutls28-dev libjansson-dev libjansson4 \
+     libjpeg-dev liblzma-dev libmagick++-dev libmagickcore-dev libncurses-dev \
+     libncurses6 libncursesw6 libpng-dev libpoppler-glib-dev \
+     libpoppler-private-dev libreadline-dev libsqlite3-dev libssl-dev \
+     libsystemd-dev libtiff-dev libwebkit2gtk-4.1-dev libxapian-dev \
+     libxcb-xfixes0-dev libxkbcommon-dev libxpm-dev llvm make mc meson mosh ncdu \
+     net-tools nnn notmuch openssh-server p7zip-full p7zip-rar pandoc pass peco \
+     pkg-config plantuml postfix pydf python3 python3-openssl python3-pip \
+     python3-pip ranger ripgrep rtv rtv ruby screen shellcheck sqlite3 stow \
+     tabbed texinfo tig tk-dev tmux trash-cli ubuntu-restricted-extras unrar \
+     wget wmctrl xdg-utils xdotool xz-utils zathura zathura-djvu \
+     zathura-pdf-poppler zlib1g-dev
 
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo add-apt-repository ppa:neovim-ppa/stable -y
