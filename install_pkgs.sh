651bdf6d58024035b1df2393898ea8911737b210
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-06-03 01:20:31 +0800

    Kitty: bind f12 to open popup terminal

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -8,7 +8,7 @@ sudo apt-get install apt-fast
 
 # install other packages
 sudo apt-fast install -y \
-     alacritty anki autojump automake bibtool btop build-essential checkinstall \
+     alacritty anki apt-file autojump automake bibtool btop build-essential checkinstall \
      chrome-gnome-shell clang cmake cmake cscope curl curl ditaa fasd fbreader \
      fd-find fish fonts-firacode fonts-jetbrains-mono fzy gcc-10 gettext git \
      git-lfs glances global gnome-tweaks gnupg2 graphviz guile-3.0-dev html2text \
@@ -24,7 +24,7 @@ sudo apt-fast install -y \
      pkg-config plantuml postfix pydf python3 python3-openssl python3-pip \
      python3-pip ranger ripgrep rtv rtv ruby screen shellcheck sqlite3 stow \
      tabbed texinfo tig tk-dev tmux trash-cli ubuntu-restricted-extras unrar \
-     wget wmctrl xdg-utils xdotool xz-utils zathura zathura-djvu \
+     wget wmctrl xbindkeys xdg-utils xdotool xz-utils zathura zathura-djvu \
      zathura-pdf-poppler zlib1g-dev
 
 sudo add-apt-repository ppa:git-core/ppa -y
