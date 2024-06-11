90eaaa0f59ff294767301747e0cbf2fa452e093f
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-06-12 05:16:23 +0800

    Emacs: add ai/llm setup

---
+++ spacemacs/config/config-ai.el
@@ -0,0 +1,31 @@
+;; This file configures llms for use.
+(defun my-ai-setup ()
+  ;; gptel settings
+  (gptel-make-ollama "Ollama"
+    :host "localhost:11434"
+    :stream t
+    :models '("llama3:8b"
+              "llama3:70b"
+              "phi3:3.8b"
+              "phi3:14b"
+              "wizardlm2:7b"
+              "wizardlm2:8x22b"
+              "command-r"
+              "command-r-plus"))
+
+  (gptel-make-openai "TogetherAI"
+    :host "api.together.xyz"
+    :key together-ai-api-key
+    :stream t
+    :models '("meta-llama/Llama-3-8b-chat-hf"
+              "meta-llama/Llama-3-70b-chat-hf"
+              "microsoft/WizardLM-2-8x22B"))
+
+  (gptel-make-openai "Deepseek"
+    :host "api.deepseek.com"
+    :key deepseek-api-key
+    :stream t
+    :models '("deepseek-chat"
+              "deepseek-coder")))
+
+(provide 'config-ai)
