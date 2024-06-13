1df92a2b57f03b00729e13ced88443509450e012
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-06-13 17:47:25 +0800

    Helix: add helix config based on spacemacs

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -213,6 +213,7 @@ sudo snap install --classic skype
 sudo snap install --classic slack
 sudo snap install --classic pycharm-professional
 sudo snap install --classic code
+sudo snap install --classic helix
 sudo snap install --classic obsidian
 sudo snap install --classic zellij
 sudo snap install logseq
