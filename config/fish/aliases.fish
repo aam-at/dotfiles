6ddac2242fc60148dcdf0afdc718dc76e3e4efcb
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-06-22 18:55:02 +0800

    Tmux: make tmux and kitty play along nicely

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -1,7 +1,7 @@
 abbr -a -- - 'cd -'
 
 # If inside a kitty terminal, add aliases for kitty's icat, ssh, diff, and hg
-if test -n "$KITTY_PID"; or begin; tmux info > /dev/null 2>&1; and test -n (tmux showenv -g KITTY_PID 2>/dev/null | string split -f2 '='); end
+if test -n "$KITTY_PID"; or begin; tmux info > /dev/null 2>&1; and test -n (tmux showenv KITTY_PID 2>/dev/null | string split -f2 '='); end
   alias icat="kitten icat"
   alias ssh="kitty +kitten ssh"
   alias diff="kitten diff"
