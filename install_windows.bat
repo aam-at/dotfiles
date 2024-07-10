ce0e51ed0795f3210d85433acf3056b788096cfc
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-07-10 18:37:26 +0800

    Windows: refactor windows bootstrap script

--- install_windows.bat
+++
@@ -1,6 +0,0 @@
-choco install ^
-7zip adb adobereader ag anki autohotkey calibre clink conemu curl delta dropbox emacs ^
-evernote evince far ffmpeg firefox git gnupg google-backup-and-sync googlechrome ^
-gpg4win handbrake jre8 latexdraw msys2 notepadplusplus pandoc python ^
-python3 qtpass ripgrep skype spotify sqlite sysinternals telegram totalcommander ^
-utorrent viber vim virtualbox vlc wget WinFsp winrar youtube-dl
