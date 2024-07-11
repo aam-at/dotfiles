d688bf92630d61ecbe82cb7100dc37d6ec5129eb
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-07-12 05:04:26 +0800

    Mingw: add tree-sitter

--- windows/bootstrap_mingw64.sh
+++ windows/bootstrap_mingw64.sh
@@ -9,11 +9,11 @@ pacman -Syu \
         autoconf autogen automake automake-wrapper diffutils git guile libgc \
         libguile libidn-devel libltdl libnettle-devel libopenssl \
         libp11-kit-devel libtasn1-devel libunistring make \
-        mingw-w64-x86_64-wxwidgets3.2-gtk3 mingw-w64-x86_64-binutils \
-        mingw-w64-x86_64-bzip2 mingw-w64-x86_64-cairo mingw-w64-x86_64-crt-git \
-        mingw-w64-x86_64-dbus mingw-w64-x86_64-emacs-pdf-tools-server \
-        mingw-w64-x86_64-expat mingw-w64-x86_64-fontconfig \
-        mingw-w64-x86_64-freetype mingw-w64-x86_64-gcc mingw-w64-x86_64-gcc-libs \
+        mingw-w64-x86_64-binutils mingw-w64-x86_64-bzip2 mingw-w64-x86_64-cairo \
+        mingw-w64-x86_64-crt-git mingw-w64-x86_64-dbus \
+        mingw-w64-x86_64-emacs-pdf-tools-server mingw-w64-x86_64-expat \
+        mingw-w64-x86_64-fontconfig mingw-w64-x86_64-freetype \
+        mingw-w64-x86_64-gcc mingw-w64-x86_64-gcc-libs \
         mingw-w64-x86_64-gdk-pixbuf2 mingw-w64-x86_64-gettext \
         mingw-w64-x86_64-giflib mingw-w64-x86_64-glib2 mingw-w64-x86_64-gmp \
         mingw-w64-x86_64-gnutls mingw-w64-x86_64-harfbuzz \
@@ -27,6 +27,7 @@ pacman -Syu \
         mingw-w64-x86_64-libunistring mingw-w64-x86_64-libwinpthread-git \
         mingw-w64-x86_64-libxml2 mingw-w64-x86_64-mpc mingw-w64-x86_64-mpfr \
         mingw-w64-x86_64-nettle mingw-w64-x86_64-p11-kit mingw-w64-x86_64-pango \
-        mingw-w64-x86_64-pixman mingw-w64-x86_64-winpthreads \
+        mingw-w64-x86_64-pixman mingw-w64-x86_64-tree-sitter \
+        mingw-w64-x86_64-winpthreads mingw-w64-x86_64-wxwidgets3.2-gtk3 \
         mingw-w64-x86_64-xpm-nox mingw-w64-x86_64-xz mingw-w64-x86_64-zlib nano \
         openssl pkgconf tar texinfo wget
