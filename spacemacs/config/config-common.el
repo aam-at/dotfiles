cc3e4b6a503e6dee1e72b27b4e15d3eff5dc1d62
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-07-12 13:46:41 +0800

    Ubuntu: add lsp-booster

--- spacemacs/config/config-common.el
+++ spacemacs/config/config-common.el
@@ -41,6 +41,41 @@
     (spacemacs/toggle-fill-column-indicator-on)
     (spacemacs/toggle-relative-line-numbers-on))
   (add-hook 'prog-mode-hook 'my-prog-settings)
+  ;; speed up the lsp-mode using lsp-booster
+  (if (executable-find "emacs-lsp-booster")
+      (progn
+        (defun lsp-booster--advice-json-parse (old-fn &rest args)
+          "Try to parse bytecode instead of json."
+          (or
+           (when (equal (following-char) ?#)
+             (let ((bytecode (read (current-buffer))))
+               (when (byte-code-function-p bytecode)
+                 (funcall bytecode))))
+           (apply old-fn args)))
+        (advice-add (if (progn (require 'json)
+                               (fboundp 'json-parse-buffer))
+                        'json-parse-buffer
+                      'json-read)
+                    :around
+                    #'lsp-booster--advice-json-parse)
+
+        (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+          "Prepend emacs-lsp-booster command to lsp CMD."
+          (let ((orig-result (funcall old-fn cmd test?)))
+            (if (and (not test?)                             ;; for check lsp-server-present?
+                     (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+                     lsp-use-plists
+                     (not (functionp 'json-rpc-connection))  ;; native json-rpc
+                     (executable-find "emacs-lsp-booster"))
+                (progn
+                  (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+                    (setcar orig-result command-from-exec-path))
+                  (message "Using emacs-lsp-booster for %s!" orig-result)
+                  (cons "emacs-lsp-booster" orig-result))
+              orig-result)))
+        (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
+        (message "emacs-lsp-booster configuration loaded successfully."))
+    (message "emacs-lsp-booster not found. To install, run: cargo install --git https://github.com/blahgeek/emacs-lsp-booster"))
 
   ;; basic text settings
   (add-hook 'text-mode-hook 'visual-line-mode)
