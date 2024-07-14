92af937f829ab7350d699420f42098c9de656275
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-07-15 00:58:21 +0800

    Aliases: add realssh alias

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -4,6 +4,7 @@ abbr -a -- - 'cd -'
 if test -n "$KITTY_PID"; or begin; tmux info > /dev/null 2>&1; and test -n (tmux showenv KITTY_PID 2>/dev/null | string split -f2 '='); end
   alias icat="kitten icat"
   alias ssh="kitty +kitten ssh"
+  alias realssh="/usr/bin/ssh"
   alias diff="kitten diff"
   alias hg="kitten hyperlinked-grep"
 end
