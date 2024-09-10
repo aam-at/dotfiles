7ed1e56282b4c9b39a66439b4bac982d7556fc49
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-09-11 03:18:51 +0800

    Nvim: add barbar plugin

--- config/lazyvim/lua/config/keymaps.lua
+++ config/lazyvim/lua/config/keymaps.lua
@@ -2,3 +2,23 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+--- Barbar keymaps
+local opts = { noremap = true, silent = true }
+-- Move to previous/next
+vim.keymap.set("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
+vim.keymap.set("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
+-- Re-order to previous/next
+vim.keymap.set("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
+vim.keymap.set("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
+-- Goto buffer in position...
+vim.keymap.set("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
+vim.keymap.set("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
+vim.keymap.set("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
+vim.keymap.set("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
+vim.keymap.set("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
+vim.keymap.set("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
+vim.keymap.set("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
+vim.keymap.set("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
+vim.keymap.set("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
+vim.keymap.set("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
