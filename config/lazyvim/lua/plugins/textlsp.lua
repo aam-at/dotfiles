fbad0bec1cbea606047e5a22d91b294e6a69d78c
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-09-13 10:17:04 +0800

    Update textlsp settings

--- config/lazyvim/lua/plugins/textlsp.lua
+++ config/lazyvim/lua/plugins/textlsp.lua
@@ -15,7 +15,29 @@ return {
 						textLSP = {
 							analysers = {
 								languagetool = {
+									enabled = false,
+								},
+								gramformer = {
+									-- gramformer dependency needs to be installed manually
 									enabled = true,
+									gpu = true,
+									check_text = {
+										on_open = true,
+										on_save = true,
+										on_change = false,
+									},
+								},
+								openai = {
+									enabled = false,
+									api_key = "api_key",
+									-- url = '<CUSTOM_URL>'  -- optional to use an OpenAI-compatible server
+									check_text = {
+										on_open = false,
+										on_save = false,
+										on_change = false,
+									},
+									model = "gpt-4o-mini",
+									max_token = 50,
 								},
 							},
 						},
