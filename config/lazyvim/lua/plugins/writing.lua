aa4a2a83eae385a5099ea37d4c6c1c447a18030c
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-09-14 15:18:16 +0800

    Nvim: update neovim config

--- config/lazyvim/lua/plugins/writing.lua
+++ config/lazyvim/lua/plugins/writing.lua
@@ -1,3 +1,5 @@
+local _, openai_api_key = RunShellCommand("copy_password.sh apikey")
+
 return {
 	-- uncover usage problems in your writing
 	{
@@ -50,5 +52,53 @@ return {
 	{
 		"dbmrq/vim-ditto",
 		cmd = "ToggleDitto",
+	},
+	-- automatically install textlsp
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			table.insert(opts.ensure_installed, "textlsp")
+		end,
+	},
+	-- text lsp server for grammar checking
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				textlsp = {
+					filetypes = { "tex", "latex", "pandoc", "markdown" },
+					settings = {
+						textLSP = {
+							analysers = {
+								languagetool = {
+									enabled = false,
+								},
+								gramformer = {
+									-- gramformer dependency needs to be installed manually
+									enabled = true,
+									gpu = true,
+									check_text = {
+										on_open = false,
+										on_save = false,
+										on_change = false,
+									},
+								},
+								openai = {
+									enabled = true,
+									api_key = openai_api_key,
+									check_text = {
+										on_open = false,
+										on_save = false,
+										on_change = false,
+									},
+									model = "gpt-4o-mini",
+									max_token = 50,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	},
 }
