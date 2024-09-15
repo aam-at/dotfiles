02cebb8fe499b078c50cf522678d2f243d50137e
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-09-15 16:33:23 +0800

    Nvim: add window picker plugin

--- config/lazyvim/lua/plugins/extras.lua
+++ config/lazyvim/lua/plugins/extras.lua
@@ -21,4 +21,14 @@ return {
 			},
 		},
 	},
+	-- window picker
+	{
+		"s1n7ax/nvim-window-picker",
+		name = "window-picker",
+		event = "VeryLazy",
+		version = "2.*",
+		config = function()
+			require("window-picker").setup()
+		end,
+	},
 }
