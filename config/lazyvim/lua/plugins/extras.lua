d413d90bcf920945adcdb5cc82bb3c586f949d8f
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-09-25 03:31:59 +0800

    Nvim: add keybindings for zen mode

--- config/lazyvim/lua/plugins/extras.lua
+++ config/lazyvim/lua/plugins/extras.lua
@@ -2,11 +2,15 @@ return {
 	-- Oil - A modern, extensible, and fast file explorer for neovim
 	{
 		"stevearc/oil.nvim",
+		cmd = "Oil",
 		opts = {},
 		dependencies = { { "echasnovski/mini.icons", opts = {} } },
 		config = function(_, opts)
 			require("oil").setup(opts)
 		end,
+		keys = {
+			vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" }),
+		},
 	},
 	-- Vim-visual-multi - Multiple cursors in vim
 	{
@@ -15,10 +19,14 @@ return {
 	-- zen-mode - Distraction-free writing
 	{
 		"folke/zen-mode.nvim",
+		cmd = "ZenMode",
 		opts = {
 			window = {
 				width = 0.85,
 			},
+		},
+		keys = {
+			{ "<leader>uz", "<cmd>ZenMode<cr>", desc = "Zen Mode" },
 		},
 	},
 	-- window picker
