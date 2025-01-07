b2ec388b864faec754576709ef05013788522ea6
Alexander Matyasko <alexander.matyasko@gmail.com>
2025-01-07 22:18:51 +0800

    Hypr: add --focus for hdrop terminal

--- scripts/popup_terminal_hypr.sh
+++ scripts/popup_terminal_hypr.sh
@@ -4,14 +4,14 @@
 
 # Check if tdrop is installed
 if ! command -v hdrop &>/dev/null; then
-    echo "hrdrop could not be found. Please install hdrop (https://github.com/Schweber/hdrop)."
-    exit 1
+  echo "hrdrop could not be found. Please install hdrop (https://github.com/Schweber/hdrop)."
+  exit 1
 fi
 
 # Check if kitty is installed
 if ! command -v kitty &>/dev/null; then
-    echo "kitty could not be found. Please install kitty."
-    exit 1
+  echo "kitty could not be found. Please install kitty."
+  exit 1
 fi
 
 # Set default values for width and height
@@ -19,4 +19,4 @@ WIDTH="100"
 HEIGHT="50"
 CLASS=kitty-dropterm
 
-hdrop --floating --position top --gap 40 --width $WIDTH --height $HEIGHT kitty --class $CLASS
+hdrop --floating --position top --gap 40 --width $WIDTH --height $HEIGHT kitty --class $CLASS --focus
