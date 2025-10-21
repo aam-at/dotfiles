b60999dd56d69ed3b305b4bee60721ec034e820d
Alexander Matyasko <alexander.matyasko@gmail.com>
2025-10-21 20:00:47 +0800

    Nvim: update lazyvim config

--- config/lazyvim/lua/plugins/org.lua
+++ config/lazyvim/lua/plugins/org.lua
@@ -1,61 +1,100 @@
 return {
-	{
-		"nvim-orgmode/orgmode",
-		dependencies = {
-			{
-				"nvim-treesitter/nvim-treesitter",
-				config = function()
-					require("nvim-treesitter.configs").setup({
-						ensure_installed = { "norg" },
-					})
-				end,
-			},
-			{
-				"hrsh7th/nvim-cmp",
-				optional = true,
-				opts = function(_, opts)
-					table.insert(opts.sources, 1, {
-						name = "orgmode",
-					})
-				end,
-			},
-		},
-		event = "VeryLazy",
-		ft = { "org" },
-		config = function()
-			-- Setup orgmode
-			require("orgmode").setup({
-				org_agenda_files = {
-					"~/Dropbox/Org/inbox.org",
-					"~/Dropbox/Org/someday.org",
-					"~/Dropbox/Org/work.org",
-					"~/Dropbox/Org/personal.org",
-					"~/Dropbox/Org/ideas.org",
-					"~/Dropbox/Org/projects/**",
-				},
-				org_default_notes_file = "~/Dropbox/Org/refile.org",
-			})
-		end,
-	},
-	{
-		"chipsenkbeil/org-roam.nvim",
-		dependencies = {
-			{
-				"nvim-orgmode/orgmode",
-			},
-		},
-		event = "VeryLazy",
-		ft = { "org" },
-		config = function()
-			require("org-roam").setup({
-				directory = "~/Dropbox/Org/",
-			})
-		end,
-	},
-	{
-		"bi0ha2ard/telescope-org_roam.nvim",
-		requires = {
-			{ "nvim-telescope/telescope.nvim" },
-		},
-	},
+  {
+    "nvim-orgmode/orgmode",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-orgmode/telescope-orgmode.nvim",
+      "nvim-orgmode/org-bullets.nvim",
+      "danilshvalov/org-modern.nvim",
+      "Saghen/blink.cmp",
+    },
+    event = "VeryLazy",
+    ft = { "org" },
+    config = function()
+      local Menu = require("org-modern.menu")
+
+      require("orgmode").setup({
+
+        ui = {
+          menu = {
+            handler = function(data)
+              Menu:new():open(data)
+            end,
+          },
+        },
+        org_agenda_files = {
+          "~/Dropbox/Org/inbox.org",
+          "~/Dropbox/Org/someday.org",
+          "~/Dropbox/Org/work.org",
+          "~/Dropbox/Org/personal.org",
+          "~/Dropbox/Org/ideas.org",
+          "~/Dropbox/Org/projects/**",
+        },
+        org_default_notes_file = "~/Dropbox/Org/refile.org",
+      })
+      require("org-bullets").setup()
+      require("blink.cmp").setup({
+        sources = {
+          per_filetype = {
+            org = { "orgmode" },
+          },
+          providers = {
+            orgmode = {
+              name = "Orgmode",
+              module = "orgmode.org.autocompletion.blink",
+              fallbacks = { "buffer" },
+            },
+          },
+        },
+      })
+
+      require("telescope").setup()
+      require("telescope").load_extension("orgmode")
+      vim.keymap.set("n", "<leader>r", require("telescope").extensions.orgmode.refile_heading)
+      vim.keymap.set("n", "<leader>fh", require("telescope").extensions.orgmode.search_headings)
+      vim.keymap.set("n", "<leader>li", require("telescope").extensions.orgmode.insert_link)
+    end,
+  },
+  {
+    "chipsenkbeil/org-roam.nvim",
+    dependencies = {
+      {
+        "nvim-orgmode/orgmode",
+      },
+    },
+    event = "VeryLazy",
+    ft = { "org" },
+    config = function()
+      require("org-roam").setup({
+        directory = "~/Dropbox/Org/",
+      })
+    end,
+  },
+  {
+    "hamidi-dev/org-super-agenda.nvim",
+    dependencies = {
+      "nvim-orgmode/orgmode", -- required
+      { "lukas-reineke/headlines.nvim", config = true }, -- optional nicety
+    },
+
+    config = function()
+      require("org-super-agenda").setup({
+        org_files = {
+          "~/Dropbox/Org/inbox.org",
+          "~/Dropbox/Org/someday.org",
+          "~/Dropbox/Org/work.org",
+          "~/Dropbox/Org/personal.org",
+          "~/Dropbox/Org/ideas.org",
+          "~/Dropbox/Org/projects/**",
+        },
+        org_directories = { "~/Dropbox/Org/" },
+      })
+    end,
+  },
+  {
+    "bi0ha2ard/telescope-org_roam.nvim",
+    requires = {
+      { "nvim-telescope/telescope.nvim" },
+    },
+  },
 }
