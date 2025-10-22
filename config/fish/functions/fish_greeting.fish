ed9be10a4781f7b74150bf022d33b0569854a3cb
Alexander Matyasko <alexander.matyasko@gmail.com>
2025-10-22 15:48:59 +0800

    Fastfetch: update config

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -3,19 +3,8 @@ function fish_greeting
     set -l text_color (set_color --bold brwhite)
     set -l reset_color (set_color normal)
 
-    set -l banner \
-        '     ______           __          __  _       ' \
-        '    / ____/___ ____  / /__  _____/ /_(_)___ _ ' \
-        '   / /   / __ `/ _ \/ / _ \/ ___/ __/ / __ `/ ' \
-        '  / /___/ /_/ /  __/ /  __(__  ) /_/ / /_/ /  ' \
-        '  \____/\__,_/\___/_/\___/____/\__/_/\__,_/   '
-
-    for line in $banner
-        printf '%s%s%s\n' $banner_color $line $reset_color
-    end
-
     if type -q fastfetch
-        command fastfetch --key-padding-left 5
+        command fastfetch
     else
         printf '%sFastfetch not found › enjoy your shell.%s\n' $text_color $reset_color
     end
