05e75b20b3dad56e2e71664bf86e5c3aaef075d9
Alexander Matyasko <alexander.matyasko@gmail.com>
2025-10-30 01:06:52 +0800

    Nvim: update lspsaga config

--- config/lazyvim/lua/plugins/lspsaga.lua
+++ config/lazyvim/lua/plugins/lspsaga.lua
@@ -1,24 +1,36 @@
 return {
-	{
-		"nvimdev/lspsaga.nvim",
-		after = "nvim-lspconfig",
-		config = function()
-			require("lspsaga").setup({})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		opts = function()
-			local keys = require("lazyvim.plugins.lsp.keymaps").get()
-			keys[#keys + 1] = { "K", "<cmd>Lspsaga hover_doc<cr>", desc = "Show doc" }
-			keys[#keys + 1] = { "gd", "<cmd>Lspsaga goto_definition<cr>", desc = "Goto Definition" }
-			keys[#keys + 1] = { "gD", "<cmd>Lspsaga goto_type_definition<cr>", desc = "Goto Declaration" }
-			keys[#keys + 1] = { "gp", "<cmd>Lspsaga peek_definition<cr>", desc = "Peek Definition" }
-			keys[#keys + 1] = { "gP", "<cmd>Lspsaga peek_type_definition<cr>", desc = "Peek Declaration" }
-			keys[#keys + 1] = { "gr", "<cmd> Lspsaga finder<cr>", desc = "References" }
-			keys[#keys + 1] = { "gR", "<cmd> Telescope lsp_references<cr>", desc = "References", nowait = true }
-			keys[#keys + 1] = { "<leader>cO", "<cmd>Lspsaga outline<cr>", desc = "Toggle outline" }
-			keys[#keys + 1] = { "<leader>'", "<cmd>Lspsaga term_toggle<cr>", desc = "Term outline" }
-		end,
-	},
+  {
+    "nvimdev/lspsaga.nvim",
+    after = "nvim-lspconfig",
+    config = function()
+      require("lspsaga").setup({})
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- apply to all servers with ['*']
+        ["*"] = {
+          keys = {
+            { "K", "<cmd>Lspsaga hover_doc<cr>", desc = "Show doc", has = "hover" },
+            { "gd", "<cmd>Lspsaga goto_definition<cr>", desc = "Goto Definition", has = "definition" },
+            { "gD", "<cmd>Lspsaga goto_type_definition<cr>", desc = "Goto Declaration", has = "typeDefinition" },
+            { "gp", "<cmd>Lspsaga peek_definition<cr>", desc = "Peek Definition", has = "definition" },
+            { "gP", "<cmd>Lspsaga peek_type_definition<cr>", desc = "Peek Declaration", has = "typeDefinition" },
+            { "gr", "<cmd>Lspsaga finder<cr>", desc = "References", has = "references" },
+            {
+              "gR",
+              "<cmd>Telescope lsp_references<cr>",
+              desc = "References (Telescope)",
+              nowait = true,
+              has = "references",
+            },
+            { "<leader>cO", "<cmd>Lspsaga outline<cr>", desc = "Toggle outline" },
+            { "<leader>'", "<cmd>Lspsaga term_toggle<cr>", desc = "Term toggle" },
+          },
+        },
+      },
+    },
+  },
 }
