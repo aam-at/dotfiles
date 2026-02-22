1361e95909e197883972ef0d5997b4d3c3b32fa9
Alexander Matyasko <alexander.matyasko@gmail.com>
2026-02-23 02:46:32 +0800

    Nvim: add ty and pyrefly

--- config/lazyvim/lua/plugins/python.lua
+++ config/lazyvim/lua/plugins/python.lua
@@ -1,38 +1,35 @@
 return {
-	-- mason to automatically install ruff and basedpyright
-	{
-		"mason-org/mason.nvim",
-		opts = function(_, opts)
-			table.insert(opts.ensure_installed, "ruff")
-			table.insert(opts.ensure_installed, "basedpyright")
-		end,
-	},
-	-- configure ruff as a formatter for python
-	{
-		"stevearc/conform.nvim",
-		optional = true,
-		opts = {
-			formatters_by_ft = {
-				["python"] = { "ruff" },
-			},
-		},
-	},
-	-- automatically resolve imports for pyright server
-	{
-		"stevanmilic/nvim-lspimport",
-		config = function(_, opts)
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "python",
-				callback = function()
-					local fixImport = require("lspimport").import
-					vim.keymap.set(
-						"n",
-						"<leader>ci",
-						fixImport,
-						{ desc = "Fix imports", noremap = true, silent = true }
-					)
-				end,
-			})
-		end,
-	},
+  -- mason to automatically install ruff and basedpyright
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "pyrefly")
+      table.insert(opts.ensure_installed, "ty")
+      table.insert(opts.ensure_installed, "ruff")
+      table.insert(opts.ensure_installed, "basedpyright")
+    end,
+  },
+  -- configure ruff as a formatter for python
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["python"] = { "ruff" },
+      },
+    },
+  },
+  -- automatically resolve imports for pyright server
+  {
+    "stevanmilic/nvim-lspimport",
+    config = function(_, opts)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "python",
+        callback = function()
+          local fixImport = require("lspimport").import
+          vim.keymap.set("n", "<leader>ci", fixImport, { desc = "Fix imports", noremap = true, silent = true })
+        end,
+      })
+    end,
+  },
 }
