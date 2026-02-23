90854ef7161d1a3bb953704a7962c9707a2d8457
Alexander Matyasko <alexander.matyasko@gmail.com>
2026-02-23 13:03:57 +0800

    Emacs: disable pyright and use ty and pyrefly by default

--- spacemacs/config/config-python.el
+++ spacemacs/config/config-python.el
@@ -6,7 +6,22 @@
         lsp-pylsp-plugins-ruff-enabled t
         lsp-pyright-langserver-command "basedpyright"
         lsp-pyright-multi-root nil
-        lsp-disabled-clients '(semgrep-ls trunk-lsp ruff pyls pylsp))
+        lsp-disabled-clients '(semgrep-ls trunk-lsp ruff pyls pylsp pyright))
+
+  (with-eval-after-load 'lsp-mode
+    (lsp-register-client
+     (make-lsp-client
+      :new-connection (lsp-stdio-connection '("ty" "server"))
+      :activation-fn (lsp-activate-on "python")
+      :priority 1
+      :server-id 'ty))
+
+    (lsp-register-client
+     (make-lsp-client
+      :new-connection (lsp-stdio-connection '("pyrefly" "lsp"))
+      :activation-fn (lsp-activate-on "python")
+      :priority 2
+      :server-id 'pyrefly)))
 
   (reformatter-define ruff-isort
       :program ruff-format-command
